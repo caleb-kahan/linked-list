14524014eec56f87280ee9f53268960066e807f4
caleb-kahan <47285759+caleb-kahan@users.noreply.github.com>
2019-10-24 00:21:06 -0400

    adding insert_front

--- main.c
+++ main.c
@@ -1,17 +1,37 @@
+
 #include <stdio.h>
-struct node { int i; struct node *next; };
+#include <stdlib.h> 
+
+
+struct node { int i; struct node *next;};
+struct node * insert_front(struct node *nody, int data);
 
 void print_list(struct node *nody){
     printf("Printing list: [ ");
-    while(nody->i){
-      printf("%d ", nody -> i);
+    while(nody->next){
+      printf("%d ", nody->i);
       nody = nody->next;
     }
+    printf("%d ", nody->i);
     printf("]\n");
 }
 int main(){
+  struct node *list = calloc(10,sizeof(struct node)); 
   struct node nody;
   nody.i = 1;
-  print_list(&nody);
+  list[0] = nody;
+  
+  
+  list = insert_front(list,99);
+  print_list(list);
   return 0;
+}
+struct node * insert_front(struct node *nody, int data){
+    struct node newNode;
+    newNode.i = data;
+    newNode.next = nody;
+    
+    struct node *longNody = realloc(nody,sizeof(nody)+sizeof(struct node));
+    longNody[0] = newNode;
+    return longNody;
 }
