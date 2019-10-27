07e12a2ed1ad635fd0e8097ccc53774a89bdd875
Caleb Kahan <ckahan00@stuy.edu>
2019-10-26 22:50:53 -0400

    finsihsed everything

--- main.c
+++ main.c
@@ -29,12 +29,12 @@ int main(){
   //list = remove_node(list,1);
   list = remove_node(list,99);
   print_list(list);
-  list = remove_node(list,1);
+  //list = remove_node(list,1);
   print_list(list);
   list = insert_front(list,1);
   list = insert_front(list,99);
   print_list(list);
-  free_list(list);
+  list = free_list(list);
   print_list(list);
   return 0;
 }
