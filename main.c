7592f6348d99b9ea881da9a77b27149aef6cd9b6
Caleb Kahan <ckahan00@stuy.edu>
2019-10-27 02:10:17 -0400

    added bunch of files

--- main.c
+++ main.c
@@ -1,24 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "header.h"
 
 struct node { int i;
               struct node *next;
             };
-struct node *insert_front(struct node *nody, int data);
-struct node *remove_node(struct node *front, int data);
-struct node * free_list(struct node *list);
-void print_list(struct node *nody){
-    printf("Printing list: [ ");
-    if(nody != NULL){
-      while(nody->next){
-        printf("%d ", nody->i);
-        nody = nody->next;
-      }
-      printf("%d ", nody->i);
-    }
-    printf("]\n");
-}
+
 int main(){
   struct node *list = malloc(sizeof(struct node));
   list->i = 1;
@@ -37,36 +24,4 @@ int main(){
   list = free_list(list);
   print_list(list);
   return 0;
-}
-struct node * insert_front(struct node *nody, int data){
-    struct node *newNode = malloc(sizeof(struct node));
-    newNode->i = data;
-    newNode->next = nody;
-    return newNode;
-}
-
-struct node *remove_node(struct node *front, int data){
-  if(front->i == data){
-      struct node *returner = front->next;
-      free(front);
-      return returner;
-  }
-
-  struct node *currentNode = front;
-  while(currentNode){
-    if(currentNode->next->i == data){
-        struct node *deadNode = currentNode->next;
-        currentNode->next = currentNode->next->next;
-        free(deadNode);
-        return front;
-    }
-    currentNode = currentNode->next;
-  }
-  return front;
-}
-struct node * free_list(struct node *list){
-  while(list != NULL){
-    list = remove_node(list,list->i);
-  }
-  return list;
 }
